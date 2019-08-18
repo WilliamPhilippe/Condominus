2c2a3597c89cf9567f33cdf989452ea9ffd51324
William Philippe <wplb@ic.ufal.br>
2019-08-18 02:56:14 -0300

    to sleep

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -2,11 +2,13 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import React from 'react';
 
 import Home from './componets/home/home';
+import Access from './componets/access/access';
 
 export default function Routes(){
     return (
         <BrowserRouter>
-            <Route path="/" exact component={Home} />       
+            <Route path="/" exact component={Home} />
+            <Route path="/access/:type" component={Access} />       
         </BrowserRouter>
     );
 }
